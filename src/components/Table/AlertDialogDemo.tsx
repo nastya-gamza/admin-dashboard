6ae48ed10e4db5fb35574355ad551a2609af6dd8
nastya-gamza <nastyagamza2000@gmail.com>
2023-11-05 15:00:36 +0300

    add: alert dialog

---
+++ src/components/Table/AlertDialogDemo.tsx
@@ -0,0 +1,38 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
+interface AlertDialogDemoProps {
+  action: JSX.Element;
+  text: string;
+  onClick: () => void;
+}
+
+export function AlertDialogDemo({ action, text, onClick }: AlertDialogDemoProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant='ghost'>{action}</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>This action cannot be undone. {text}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onClick}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- src/components/Table/data-table-row-actions.tsx
+++ src/components/Table/data-table-row-actions.tsx
@@ -1,48 +1,28 @@
-import { Row } from "@tanstack/react-table";
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  // DropdownMenuLabel,
-  // DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { MoreHorizontal } from "lucide-react";
-import { useDeleteCustomerMutation } from "@/redux";
-import { Customer } from "@/types";
+import { Row } from '@tanstack/react-table';
+import { Trash2 } from 'lucide-react';
+import { useDeleteCustomerMutation } from '@/redux';
+import { Customer } from '@/types';
+import { AlertDialogDemo } from './AlertDialogDemo';
 
 interface DataTableRowActionsProps<TData extends Customer> {
-  row: Row<TData>
+  row: Row<TData>;
 }
 
 export function DataTableRowActions<TData extends Customer>({
   row,
 }: DataTableRowActionsProps<TData>) {
-
+  
   const [deleteCustomer] = useDeleteCustomerMutation();
 
-  const handleDeleteCustomer = async(id:number) => {
-    await deleteCustomer(id).unwrap()
-  }
+  const handleDeleteCustomer = async (id: number) => {
+    await deleteCustomer(id).unwrap();
+  };
 
   return (
-    <DropdownMenu>
-    <DropdownMenuTrigger asChild>
-      <Button variant='ghost' className='h-8 w-8 p-0'>
-        <span className='sr-only'>Open menu</span>
-        <MoreHorizontal className='h-4 w-4' />
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent align='end'>
-      {/* <DropdownMenuLabel>Actions</DropdownMenuLabel> */}
-      <DropdownMenuItem onClick={() => handleDeleteCustomer(row.original.id)}>
-        Delete
-      </DropdownMenuItem>
-      {/* <DropdownMenuSeparator /> */}
-      <DropdownMenuItem>View customer</DropdownMenuItem>
-      <DropdownMenuItem>View payment details</DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
-  )
+    <AlertDialogDemo
+      action={<Trash2 />}
+      onClick={() => handleDeleteCustomer(row.original.id)}
+      text='This will permanently delete selected customer.'
+    />
+  );
 }
